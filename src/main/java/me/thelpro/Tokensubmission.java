67925a803150409ae7259d4e09eecf21ce7d4176
TheLPro <92855806+TheLPro@users.noreply.github.com>
2022-11-26 19:40:03 +0100

    Add files via upload

---
+++ src/main/java/me/thelpro/Tokensubmission.java
@@ -0,0 +1,31 @@
+package me.thelpro;
+
+import me.thelpro.commands.tokenCommand;
+import me.thelpro.events.inventoryClickEvent;
+import me.thelpro.events.playerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tokensubmission extends JavaPlugin {
+
+    private static Tokensubmission plugin;
+
+    @Override
+    public void onEnable() {
+
+        plugin = this;
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        new me.thelpro.placeholders.placeholders().register();
+        getCommand("token").setExecutor(new tokenCommand());
+        getServer().getPluginManager().registerEvents(new playerJoinEvent(), this);
+        getServer().getPluginManager().registerEvents(new inventoryClickEvent(), this);
+
+    }
+
+
+    public static Tokensubmission getPlugin() {
+        return plugin;
+    }
+
+}
